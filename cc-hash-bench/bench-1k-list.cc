34e845d65b323e949a22e32f9559d4f74a4693cc
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-03-10 11:58:09 -0800

    add list bench to c++

---
+++ cc-hash-bench/bench-1k-list.cc
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+
+#include "wyhash.h"
+
+// This is not a super robust benchmark. Just copying what we do in roc.
+constexpr int SIZE = 1024;
+
+int main() {
+  uint64_t iters;
+  std::cin >> iters;
+
+  uint64_t seed = 0x1234567890ABCDEF;
+  uint8_t data[SIZE];
+  std::fill(data, data + SIZE, 0x77);
+  int64_t hash = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int64_t remaining = iters; remaining > 0; --remaining) {
+    data[0] = remaining & 0xFF;
+    int64_t data_hash = wyhash(&data, SIZE, seed, _wyp);
+    hash = wyhash64(hash, data_hash);
+  }
+  std::cout << hash << '\n';
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+          .count();
+
+  std::cout << "runtime: " << duration / 1000.0 << "ms" << '\n';
+}
--- cc-hash-bench/build-bench.sh
+++ cc-hash-bench/build-bench.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 clang++ bench.cc -o bench -O3 -std=c++17 -stdlib=libc++ -g
+clang++ bench-1k-list.cc -o bench-1k-list -O3 -std=c++17 -stdlib=libc++ -g
