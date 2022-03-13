70ac090073cc3494ebc3453865a0dc1fb25251f3
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-03-12 22:19:09 -0800

    fix bash script for older meson

--- cc-hash-bench/build-bench.sh
+++ cc-hash-bench/build-bench.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 (cd $SCRIPT_DIR && \
     ([ -d build ] || meson setup build --buildtype release) && \
-    meson compile -C build)
+    ninja -C build)
